01669308559a792f0922aba45d6060748419bcf7
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2023-09-17 18:52:31 +0200

    Docker modified

--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.NODE_PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Share your skills server");
