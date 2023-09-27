695dcc5c1189d00019e580cd641fe28163981c29
RobinCanis <robin.canis@gmail.com>
2023-09-27 13:58:49 +0200

    putting the test back

--- back_end/__tests__/check_test.ts
+++ back_end/__tests__/check_test.ts
@@ -1,7 +1,5 @@
-// this created errors so i commented it out
-
-// describe("Check testing", () => {
-//   test("1=1", () => {
-//     expect("1").toEqual("1");
-//   });
-// });
+describe("Check testing", () => {
+  test("1=1", () => {
+    expect("1").toEqual("1");
+  });
+});
