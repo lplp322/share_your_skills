923b8e411b9e0f32db30bde3e38ac508866aac5d
Abdirahmanroble <aromar@kth.se>
2023-10-09 17:55:14 +0200

    added event card component which will be reused

--- front_end/share_your_skills/lib/views/event_page.dart
+++ front_end/share_your_skills/lib/views/event_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share_your_skills/components/event_card.dart';
+import 'package:share_your_skills/views/profile_page.dart';
+
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
@@ -10,36 +13,13 @@ class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(height: 30,),
-          Row(
-            // create two rows one for an post image and one for post name and date and place
-            children: [
-              // post image
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-                // get image from images folder
-               child: Image.asset(
-                 'images/gardening.webp',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              // post name and date and place
-              Column(
-                children: [
-                  Text('Post Name'),
-                  Text('Post Date'),
-                  Text('Post Place'),
-                ],
-              ),
-            ],
+        child: EventCard(
+           title: 'Gardening',
+           location: 'Solna',
+            date: '21 Oct',
+            onTap: () {
 
-          ),
-        ],
-      ),
-    );
+          },
+    ));
   }
 }
