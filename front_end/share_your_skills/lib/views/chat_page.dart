65798506626f24858a26cd43dbba75e18cf4ca36
Abdirahmanroble <aromar@kth.se>
2023-10-25 16:32:52 +0200

    final touch

--- front_end/share_your_skills/lib/views/chat_page.dart
+++ front_end/share_your_skills/lib/views/chat_page.dart
@@ -11,7 +11,7 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Chat Page'),
+      child: Text('Coming soon...'),
     );
   }
 }
